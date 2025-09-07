b2b048ef6cdc470603fac62eb12e1ba008fca997
Vishal Kumar Sahu <vishalkumarsahu@zohomail.in>
2025-09-07 23:11:30 +0530

    Update SystemStatus.js

--- frontend/src/components/SystemStatus.js
+++ frontend/src/components/SystemStatus.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useState, useEffect } from 'react';
+
 import { checkSystemHealth } from '../utils/api';
 import './SystemStatus.css';
 
